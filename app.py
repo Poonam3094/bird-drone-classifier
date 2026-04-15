529c423280cb9ceecc4051a36937d36740138750
Poonam Saini <poonam3094@gmail.com>
2026-04-16 01:19:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,35 @@
 import streamlit as st
 import numpy as np
-from tensorflow.keras.models import load_model
 from PIL import Image
+import tensorflow as tf
 
-# Load model
-model = load_model("bird_drone_model.keras")
+# Load TFLite model
+interpreter = tf.lite.Interpreter(model_path="model.tflite")
+interpreter.allocate_tensors()
 
-st.title("?? Bird vs Drone Classifier")
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
-uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
+st.title("🦅 Bird vs Drone Classifier")
+
+uploaded_file = st.file_uploader("Upload an image", type=["jpg","png","jpeg"])
 
 if uploaded_file is not None:
 
-    img = Image.open(uploaded_file).resize((224, 224))
-    st.image(img, caption="Uploaded Image", use_column_width=True)
+    img = Image.open(uploaded_file).resize((224,224))
+    st.image(img, caption="Uploaded Image")
 
     img_array = np.array(img) / 255.0
-    img_array = np.expand_dims(img_array, axis=0)
+    img_array = np.expand_dims(img_array, axis=0).astype(np.float32)
 
-    prediction = model.predict(img_array)
+    interpreter.set_tensor(input_details[0]['index'], img_array)
+    interpreter.invoke()
+
+    prediction = interpreter.get_tensor(output_details[0]['index'])
 
     if prediction[0][0] > 0.5:
-        st.success("?? Drone Detected")
+        st.success("🚁 Drone Detected")
         st.write(f"Confidence: {prediction[0][0]:.2f}")
     else:
-        st.success("?? Bird Detected")
+        st.success("🐦 Bird Detected")
         st.write(f"Confidence: {1 - prediction[0][0]:.2f}")
